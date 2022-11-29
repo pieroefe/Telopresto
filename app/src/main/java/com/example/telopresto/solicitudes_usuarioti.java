f98f162e743982370f99c5da0c493de2a6136dde
NicoG <guillen.nicole@pucp.edu.pe>
2022-11-29 00:46:50 -0500

    admin-avance3

---
+++ app/src/main/java/com/example/telopresto/solicitudes_usuarioti.java
@@ -0,0 +1,14 @@
+package com.example.telopresto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class solicitudes_usuarioti extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitudes_usuarioti);
+    }
+}
