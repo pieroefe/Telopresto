f8838789bf894272bf01119b8e386682704b2c53
Stephania1807 <stephania.guzman@pucp.edu.pe>
2022-11-28 21:08:00 -0500

    splash

---
+++ app/src/main/java/com/example/telopresto/SplashSreen.java
@@ -0,0 +1,27 @@
+package com.example.telopresto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashSreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashSreen.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        }, 2000);
+    }
+}
