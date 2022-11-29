226b52725d698310434c36d3dcb0c326bb750bbc
Stephania1807 <stephania.guzman@pucp.edu.pe>
2022-11-28 23:48:14 -0500

    splash

---
+++ app/src/main/java/com/example/telopresto/WhatIs.java
@@ -0,0 +1,14 @@
+package com.example.telopresto;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WhatIs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.what_is);
+    }
+}
