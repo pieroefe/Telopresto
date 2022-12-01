42d6acf41e6250b65dbec2f6d5f74c9c6c225ee1
NicoG <guillen.nicole@pucp.edu.pe>
2022-11-30 20:41:26 -0500

    menuAdmin

--- app/src/main/java/com/example/telopresto/Admin/agregar_usuarioti_admin.java
+++ app/src/main/java/com/example/telopresto/Admin/agregar_usuarioti_admin.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.example.telopresto.reportesAdmin;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
--- app/src/main/java/com/example/telopresto/Admin/listadoUsuarioAdmin.java
+++ app/src/main/java/com/example/telopresto/Admin/listadoUsuarioAdmin.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.example.telopresto.R;
-import com.example.telopresto.reportesAdmin;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
