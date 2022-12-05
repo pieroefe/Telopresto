d9000b32369ba99912910f7d0554f4bbf3a915e6
Stephania1807 <stephania.guzman@pucp.edu.pe>
2022-12-05 17:26:12 -0500

    Buenas tardes

---
+++ app/src/main/java/com/example/telopresto/TI/Solicitudes_detalle.java
@@ -0,0 +1,16 @@
+package com.example.telopresto.TI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.telopresto.R;
+
+public class Solicitudes_detalle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitudes_detalle);
+    }
+}
